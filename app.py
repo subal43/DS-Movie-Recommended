e3eb79f24c16680dfe51f35610877266870b7150
subal43 <subalkundu3@gmail.com>
2025-12-31 22:55:04 +0530

    receiving list of movies

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 import streamlit as st
 import pandas as pd
+import pickle
+
+
+def recommend(movie):
+    l = []
+    movie_index = movies[movies['title'] == movie].index[0]
+    movie_list = sorted(list(enumerate(similarity[movie_index])),reverse=True , key = lambda x : x[1])[1:6]
+    for i in movie_list :
+        l.append(movies.iloc[i[0]].title)
+    return l
+
+
+movies_dict = pickle.load(open('movie.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
 
 st.title("Movie Recommendation System")
-st.selectbox("Select a Movie ")
+selected_movies = st.selectbox("Select a Movie ", movies['title'].values)
+x =  recommend(selected_movies)
+
+
+if st.button("Recommend"):
+    st.subheader("Movies you may like:")
+    for i in x:
+        st.write(i)
